4fb35e0b628380d8387891663c080e417def11d9
Rafał Duraj <durajrafal@gmail.com>
2023-03-09 12:34:18 +0100

    Add service configuration for infrastructure layer

---
+++ IssueTracker.Infrastructure/ConfigureServices.cs
@@ -0,0 +1,25 @@
+﻿using IssueTracker.Infrastructure.Persistance;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssueTracker.Infrastructure
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
+    }
+}
---
+++ IssueTracker.Infrastructure/Persistance/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using IssueTracker.Application.Common.Interfaces;
+using IssueTracker.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssueTracker.Infrastructure.Persistance
+{
+    public class AppDbContext : DbContext, IApplicationDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Project> Projects => Set<Project>();
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
