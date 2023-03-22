37b622860da6eeb613dd795ce28a0ea3626fb1e6
Rafał Duraj <durajrafal@gmail.com>
2023-03-22 11:41:32 +0100

    Add view for login action

---
+++ IssueTracker.UI/Models/Account/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IssueTracker.UI.Models.Account
+{
+    public class LoginViewModel
+    {
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public bool IsRememberChecked { get; set; }
+    }
+}
