168ebe29a666559ea46c483bfdd388e32b119dd0
Rafał Duraj <durajrafal@gmail.com>
2023-04-03 12:32:05 +0200

    Make BaseTestWithScope inherit from BaseTest

--- IssueTracker.IntegrationTests.Library/Common/BaseTestWithScope.cs
+++ IssueTracker.IntegrationTests.Library/Common/BaseTestWithScope.cs
@@ -9,23 +9,19 @@ using Xunit;
 
 namespace IssueTracker.IntegrationTests.Library.Common
 {
-    public class BaseTestWithScope : IClassFixture<CustomWebApplicationFactory>
+    public class BaseTestWithScope : BaseTest
     {
-        public readonly CustomWebApplicationFactory _factory;
-        public readonly TestingHelpers _testing;
         public readonly IServiceScopeFactory _scopeFactory;
 
         public BaseTestWithScope(CustomWebApplicationFactory factory)
+            :base(factory)
         {
-            _factory = factory;
-            _testing = new TestingHelpers(_factory);
             _scopeFactory = _factory.Services.GetRequiredService<IServiceScopeFactory>();
         }
 
         public BaseTestWithScope()
+            :base()
         {
-            _factory = new CustomWebApplicationFactory();
-            _testing = new TestingHelpers(_factory);
             _scopeFactory = _factory.Services.GetRequiredService<IServiceScopeFactory>();
         }
     }
