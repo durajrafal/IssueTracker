0101086342658253a2715c06a071e5fba7c83a7e
Rafał Duraj <durajrafal@gmail.com>
2023-04-03 12:48:19 +0200

    Add Email controller Confirm action tests

---
+++ IssueTracker.UI.IntegrationTests/Controllers/EmailTests.cs
@@ -0,0 +1,78 @@
+﻿using IssueTracker.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using IssueTracker.UI.Areas.Identity.Controllers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Moq;
+
+namespace IssueTracker.UI.IntegrationTests.Controllers
+{
+    public class EmailTests : BaseTestWithScope
+    {
+        private EmailController _controller;
+        public EmailTests(CustomWebApplicationFactory factory)
+            : base(factory)
+        {
+            var httpContext = new DefaultHttpContext();
+            var tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>());
+            _controller = new EmailController()
+            {
+                TempData = tempData
+            };
+        }
+
+        [Fact]
+        public async Task Confirm_WhenUserExistsAndTokenIsValid_ShouldConfirmEmail()
+        {
+            //Arrange
+            var user = new ApplicationUser("confirm@test.com", "John", "Smith");
+            string token;
+            using (var userManager = _scopeFactory.CreateScope().ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>())
+            {
+                await userManager.CreateAsync(user, "Pass123");
+                token = await userManager.GenerateEmailConfirmationTokenAsync(user);
+            }
+
+            //Act
+            RedirectToActionResult result;
+            using (var userManager = _scopeFactory.CreateScope().ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>())
+            {
+                var actionResult = await _controller.Confirm(token, user.Email, userManager);
+                result = actionResult as RedirectToActionResult;
+            }
+
+            //Assert
+            Assert.Equal("Account", result.ControllerName);
+            Assert.Equal("Login", result.ActionName);
+            Assert.NotNull(_controller.TempData["EmailActionSuccess"]);
+        }
+
+        [Fact]
+        public async Task Confirm_WhenUserExistsButTokenIsInvalid_ShouldRedirectToRegister()
+        {
+            //Arrange
+            var user = new ApplicationUser("confirmfail@test.com", "John", "Smith");
+            string token = Guid.NewGuid().ToString();
+            using (var userManager = _scopeFactory.CreateScope().ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>())
+            {
+                await userManager.CreateAsync(user, "Pass123");
+            }
+
+            //Act
+            RedirectToActionResult result;
+            using (var userManager = _scopeFactory.CreateScope().ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>())
+            {
+                var actionResult = await _controller.Confirm(token, user.Email, userManager);
+                result = actionResult as RedirectToActionResult;
+            }
+
+            //Assert
+            Assert.Equal("Account", result.ControllerName);
+            Assert.Equal("Register", result.ActionName);
+            Assert.NotNull(_controller.TempData["EmailActionError"]);
+        }
+
+    }
+}
--- IssueTracker.UI/Areas/Identity/Controllers/EmailController.cs
+++ IssueTracker.UI/Areas/Identity/Controllers/EmailController.cs
@@ -9,7 +9,7 @@ namespace IssueTracker.UI.Areas.Identity.Controllers
     [Area("Identity")]
     public class EmailController : Controller
     {
-
+        #region Confirm
         [HttpGet]
         public async Task<IActionResult> Confirm(string token, string email, [FromServices] UserManager<ApplicationUser> userManager)
         {
@@ -23,7 +23,7 @@ namespace IssueTracker.UI.Areas.Identity.Controllers
             if (result.Succeeded)
             {
                 var html = "<h4>Congratulations!</h4>Your email is confirmed. Now you can log in with your account.";
-                TempData["EmailConfirmSuccess"] = html;
+                TempData["EmailActionSuccess"] = html;
                 return RedirectToAction("Login", "Account");
             }
             else
@@ -34,7 +34,10 @@ namespace IssueTracker.UI.Areas.Identity.Controllers
 
         private IActionResult RedirectToRegisterWithConfirmationError(string email)
         {
-            TempData["EmailConfirmError"] = $"Impossible to confirm your email '{email}'.<strong> Register again.</strong>";
+            TempData["EmailActionError"] = $"Impossible to confirm your email '{email}'.<strong> Register again.</strong>";
+            return RedirectToAction("Register", "Account");
+        }
+        #endregion
             return RedirectToAction("Register", "Account");
         }
     }
