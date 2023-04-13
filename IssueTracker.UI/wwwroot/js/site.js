d96ae337ee83761153b1d5cf3f743dd914252934
Rafał Duraj <durajrafal@gmail.com>
2023-04-13 17:11:11 +0200

    Automatically make selected navbar link active

--- IssueTracker.UI/wwwroot/js/site.js
+++ IssueTracker.UI/wwwroot/js/site.js
@@ -1,4 +1,11 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const navbarLinks = document.querySelectorAll("#navbarMain > ul:nth-child(1) > li > a")
 
-// Write your JavaScript code.
+document.addEventListener('DOMContentLoaded', () => {
+    navbarLinks.forEach(link => {
+        if (link.getAttribute('href').toLowerCase() === location.pathname.toLowerCase()) {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active');
+        }
+    });
+})
