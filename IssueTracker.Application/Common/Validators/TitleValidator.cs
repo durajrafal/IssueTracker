0b386f7d7bac3e5e707d93f19bb71e56d6334691
Rafał Duraj <durajrafal@gmail.com>
2023-08-11 20:44:13 +0200

    Update title validator to use regex

--- IssueTracker.Application/Common/Validators/TitleValidator.cs
+++ IssueTracker.Application/Common/Validators/TitleValidator.cs
@@ -16,13 +16,11 @@ namespace IssueTracker.Application.Common.Validators
         {
             _ctx = ctx;
             const int MAX_TITLE_LENGTH = 100;
-            var forbiddenCharacters = new List<char> { ':', '/', '?', '#', '[', ']', '@', '!', '$', '&', '\'', '(', ')', '*', '+', ',', ';', '=' };
             RuleFor(x => x.Title)
                 .Cascade(CascadeMode.Stop)
                 .NotNull().NotEmpty().WithMessage("Title is required.")
                 .MaximumLength(MAX_TITLE_LENGTH).WithMessage($"Title must not exceed {MAX_TITLE_LENGTH}.")
-                .Must(title => title.All(ch => !forbiddenCharacters.Contains(ch)))
-                .WithMessage("Must not contain any of the following characters: ':', '/', '?', '#', '[', ']', '@', '!', '$', '&', '\'', '(', ')', '*', '+', ',', ';', '='")
+                .Matches("^[A-Za-z0-9_ ]+$").WithMessage("Must not contain any of the special characters.")
                 .Must(BeUniqueTitle).WithMessage("Project with the same name already exists.");
             _ctx = ctx;
         }
