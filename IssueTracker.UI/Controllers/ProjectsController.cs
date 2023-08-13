33f0ac389b682c9c96a21d82d6a134d642b42700
Rafał Duraj <durajrafal@gmail.com>
2023-08-14 00:52:31 +0200

    Add navigation to project details view

---
+++ IssueTracker.UI/Controllers/ProjectsController.cs
@@ -0,0 +1,24 @@
+﻿using IssueTracker.Application.Projects.Queries.GetProjectDetails;
+using IssueTracker.Application.Projects.Queries.GetProjects;
+using IssueTracker.UI.Models.Projects;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IssueTracker.UI.Controllers
+{
+    public class ProjectsController : ControllerWithMediatR
+    {
+        [HttpGet("/project/{id}")]
+        public async Task<IActionResult> Details (int id)
+        {
+            var query = new GetProjectDetailsQuery { ProjectId = id };
+            var result = await Mediator.Send(query);
+            var vm = new ProjectViewModel()
+            {
+                Id = result.Id,
+                Title = result.Title,
+                Issues = result.Issues
+            };
+            return View(vm);
+        }
+    }
+}
---
+++ IssueTracker.UI/Models/Projects/ProjectViewModel.cs
@@ -0,0 +1,12 @@
+﻿using IssueTracker.Domain.Entities;
+
+namespace IssueTracker.UI.Models.Projects
+{
+    public class ProjectViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public ICollection<Issue> Issues { get; init; } = new List<Issue>();
+
+    }
+}
