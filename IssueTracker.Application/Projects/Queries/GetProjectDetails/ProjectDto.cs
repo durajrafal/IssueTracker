a99d794c5631bba5a23fcbdd7f9e7ae8286264c4
Rafał Duraj <durajrafal@gmail.com>
2023-08-16 02:36:59 +0200

    Add dto for GetProjectDetails query

---
+++ IssueTracker.Application/Projects/Queries/GetProjectDetails/ProjectDto.cs
@@ -0,0 +1,17 @@
+﻿using IssueTracker.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssueTracker.Application.Projects.Queries.GetProjectDetails
+{
+    public class ProjectDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public IEnumerable<Member> Members { get; set; }
+        public ICollection<Issue> Issues { get; set; }
+    }
+}
