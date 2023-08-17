b4c7c0bbe3bf3c16733d545fc0a62734461c3c82
Rafał Duraj <durajrafal@gmail.com>
2023-08-17 16:09:54 +0200

    Add working status enum to Issue entity

--- IssueTracker.Domain/Entities/Issue.cs
+++ IssueTracker.Domain/Entities/Issue.cs
@@ -1,9 +1,4 @@
 ﻿using IssueTracker.Domain.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IssueTracker.Domain.Entities
 {
@@ -13,7 +8,8 @@ namespace IssueTracker.Domain.Entities
         public string Title { get; set; }
         public string? Description { get; set; }
         public PriorityLevel Priority { get; set; } = PriorityLevel.None;
-        public IList<Member> Members { get; init; } = new List<Member>();
+        public WorkingStatus Status { get; set; } = WorkingStatus.Pending;
+        public ICollection<Member> Members { get; set; } = new List<Member>();
         public int ProjectId { get; set; }
         public Project Project { get; set; }
     }
