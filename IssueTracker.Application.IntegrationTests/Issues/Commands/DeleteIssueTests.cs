caeacfa8b82fbdb33eecfb1bffb0257d4e40998d
Rafał Duraj <durajrafal@gmail.com>
2023-09-06 20:16:35 +0200

    Add delete issue command

---
+++ IssueTracker.Application.IntegrationTests/Issues/Commands/DeleteIssueTests.cs
@@ -0,0 +1,54 @@
+﻿using IssueTracker.Application.Common.Exceptions;
+using IssueTracker.Application.Issues.Commands.DeleteIssue;
+
+namespace IssueTracker.Application.IntegrationTests.Issues.Commands
+{
+    public class DeleteIssueTests : TestsWithMediatorFixture
+    {
+        public DeleteIssueTests() : base()
+        {
+
+        }
+
+        [Fact]
+        public async Task Handle_WhenIdIsValid_DeleteProjectFromDatabase()
+        {
+            //Arrange
+            var project = await SetupTestProjectAsync();
+            var issue = project.Issues.First();
+
+            //Act
+            var command = new DeleteIssue(issue.Id);
+            await Mediator.Send(command);
+
+            //Assert
+            Database.Func(ctx => ctx.Issues.FirstOrDefault(x => x.Id == issue.Id)).Should().BeNull();
+        }
+
+        [Fact]
+        public async Task Handle_WhenIdIsInvalid_ShouldThrowNotFoundException()
+        {
+           //Act
+            var command = new DeleteIssue(0);
+            var act = async () => await Mediator.Send(command);
+
+            //Assert
+            await act.Should().ThrowAsync<NotFoundException>();
+        }
+
+        [Fact]
+        public async Task Handle_WhenCurrentUserIsNotMember_ShouldThrowUnauthorizedAccessException()
+        {
+            //Arrange
+            var project = await SetupTestProjectAsync(addCurrentUser: false);
+            var issue = project.Issues.First();
+
+            //Act
+            var command = new DeleteIssue(issue.Id); 
+            var act = async () => await Mediator.Send(command);
+
+            //Assert
+            await act.Should().ThrowAsync<UnauthorizedAccessException>();
+        }
+    }
+}
---
+++ IssueTracker.Application/Issues/Commands/DeleteIssue/DeleteIssue.cs
@@ -0,0 +1,38 @@
+﻿using IssueTracker.Application.Common.AccessPolicies;
+using IssueTracker.Application.Common.Exceptions;
+using IssueTracker.Application.Common.Helpers;
+using IssueTracker.Application.Common.Interfaces;
+using IssueTracker.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace IssueTracker.Application.Issues.Commands.DeleteIssue
+{
+    public record DeleteIssue(int Id) : IRequest<int>;
+
+    public class DeleteIssueHandler : IRequestHandler<DeleteIssue, int>
+    {
+        private readonly IApplicationDbContext _ctx;
+        private readonly ICurrentUserService _currentUserService;
+
+        public DeleteIssueHandler(IApplicationDbContext ctx, ICurrentUserService currentUserService)
+        {
+            _ctx = ctx;
+            _currentUserService = currentUserService;
+        }
+
+        public async Task<int> Handle(DeleteIssue request, CancellationToken cancellationToken)
+        {
+            var entity = await _ctx.Issues
+                .FirstOrDefaultAsync(x => x.Id == request.Id);
+
+            entity.ApplyPolicy(new IssueCanBeAccessedOnlyByProjectMember(_ctx), _currentUserService.UserId);
+
+            if (entity is null)
+                throw new NotFoundException(nameof(Issue), request.Id.ToString());
+
+            _ctx.Issues.Remove(entity);
+            return await _ctx.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
