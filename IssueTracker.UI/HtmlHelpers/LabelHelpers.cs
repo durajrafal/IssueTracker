2b8c6b695c43ec23603ccd6a3a5e972c3bacbf9f
Rafał Duraj <durajrafal@gmail.com>
2023-09-20 20:37:27 +0200

    Update design for IssueDetails view
    * change colors
    * display member email

--- IssueTracker.UI/HtmlHelpers/LabelHelpers.cs
+++ IssueTracker.UI/HtmlHelpers/LabelHelpers.cs
@@ -8,7 +8,7 @@ namespace IssueTracker.UI.HtmlHelpers
         public static IHtmlContent LabelOverLine(this IHtmlHelper helper, string text, string boostrapIconName, string target="")
         {
             var html = $"""
-                <label for="{target}" class="fs-6 fst-italic bg-light px-2 py-1 ms-3 d-inline-block position-relative text-secondary fw-bold" 
+                <label for="{target}" class="fs-6 fst-italic bg-secondary px-2 py-1 ms-3 d-inline-block position-relative text-light fw-bold" 
                     style="top:0.5rem; z-index:1;">
                 <i class="bi {boostrapIconName} me-2"></i>{text}
                 </label>
